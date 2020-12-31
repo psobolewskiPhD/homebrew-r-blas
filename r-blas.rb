fe71288c8fa8623b9c47c97e579acca9ac134cc4
Peter Sobolewski <pete.sd@gmail.com>
2020-12-31 21:22:12 +0100

    not keg_only

--- r-blas.rb
+++ r-blas.rb
@@ -6,8 +6,6 @@ class RBlas < Formula
   license "GPL-2.0-or-later"
   revision 1
 
-  keg_only "to not clash with homebrew-core"
-
   livecheck do
     url "https://cran.rstudio.com/banner.shtml"
     regex(%r{href=(?:["']?|.*?/)R[._-]v?(\d+(?:\.\d+)+)\.t}i)
