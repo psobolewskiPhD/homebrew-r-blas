32cb176329cf72fe9c44c9faf21b933471efe5f2
Peter Sobolewski <pete.sd@gmail.com>
2020-12-31 18:44:00 +0100

    fix formula name error

--- r-blas.rb
+++ r-blas.rb
@@ -1,4 +1,4 @@
-class rblas < Formula
+class Rblas < Formula
   desc "Software environment for statistical computing, option for external BLAS"
   homepage "https://www.r-project.org/"
   url "https://cran.r-project.org/src/base/R-4/R-4.0.3.tar.gz"
